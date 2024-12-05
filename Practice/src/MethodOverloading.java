27104ca07cc21a3b0d113bfc4980e4b875861e5b
HarshVardhanK35 <kasavardhan2001@gmail.com>
2024-12-05 22:11:20 +0530

    Java- Method Overloading

--- Practice/src/MethodOverloading.java
+++ Practice/src/MethodOverloading.java
@@ -4,8 +4,15 @@ public class MethodOverloading {
         // System.out.println( "5ft, 8in = " + convertToCentimeters(5, 8) + "cm");
         // System.out.println( "68in = " + convertToCentimeters(68) + "cm");
 
-        getDurationString(0);
-        getDurationString(0, 59);
+        // getDurationString(-3945);                                    // test-1
+        // getDurationString(-65, 45);                                  // test-2
+        // getDurationString(65, 145);                     // test-3
+        getDurationString(4045);                                    // test-1
+
+        // System.out.println(area(5.0));
+        // System.out.println(area(-1));
+        // System.out.println(area(5.0, 4.0));
+        // System.out.println(area(-1.0, 4.0));
     }
 
     // challenge-1
@@ -19,25 +26,49 @@ public class MethodOverloading {
         return convertToCentimeters(totalInches);
     }
 
-
-    public static boolean getDurationString (int sec) {
-        // "XXh YYm ZZs"
-        boolean state = false;
-        if (sec >= 0) {
-            state = true;
-            return state;
+    // challenge-2
+    public static void getDurationString (int seconds) {
+        if (seconds < 0) {
+            System.out.println("Invalid input: " + (seconds) + " \nSeconds must be greater than zero");
         }
-        return state;
-    }
-    public static boolean getDurationString (int min, int sec) {
-
-        boolean state = false;
-
-        if (min >= 0 && (sec >= 0 || sec <= 59)) {
-            state = true;
-            return state;
+        else {
+            getDurationString(seconds / 60, seconds % 60);
         }
-        return state;
     }
+    public static void getDurationString (int minutes, int seconds) {
+
+        if ((minutes < 0)) {
+            System.out.println("Invalid input: " + (minutes) + " \nMinutes must be greater than zero");
+        }
+        else if ((seconds < 0 || seconds > 59) ) {
+            System.out.println("Invalid input: " + (seconds) + " \nSeconds must be between 0 and 59");
+        }
+        else {
+            int hours = minutes / 60;
+            int remainMinutes = minutes % 60;
+
+            System.out.println(hours + "h " + remainMinutes + "m " + seconds + "s");
+        }
+    }
+
+    // Udemy-Tests
+    // ---------------------------------------------------------------------------------------------------------
+    // test-1 areas of circle and rectangle
+    public static double area (double radius) {
+        if (radius < 0) {
+            return -1.0;
+        }
+        return (Math.PI * (radius * radius));
+    }
+    public static double area (double x, double y) {
+        if (x < 0 || y < 0){
+            return -1.0;
+        }
+        return (x * y);
+    }
+
+    // test-2
+    // Minutes To Years and Days Calculator
+    
 }
 
