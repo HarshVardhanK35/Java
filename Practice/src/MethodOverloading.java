774c31468cea6c52ee8ae29a849e86495ca72396
HarshVardhanK35 <kasavardhan2001@gmail.com>
2024-12-08 23:10:49 +0530

    Java Method OverRiding Completed

--- Practice/src/MethodOverloading.java
+++ Practice/src/MethodOverloading.java
@@ -7,12 +7,14 @@ public class MethodOverloading {
         // getDurationString(-3945);                                    // test-1
         // getDurationString(-65, 45);                                  // test-2
         // getDurationString(65, 145);                     // test-3
-        getDurationString(4045);                                    // test-1
+        // getDurationString(4045);                                    // test-1
 
         // System.out.println(area(5.0));
         // System.out.println(area(-1));
         // System.out.println(area(5.0, 4.0));
         // System.out.println(area(-1.0, 4.0));
+
+        printYearsAndDays(561600);
     }
 
     // challenge-1
@@ -69,6 +71,44 @@ public class MethodOverloading {
 
     // test-2
     // Minutes To Years and Days Calculator
-    
+    public static void printYearsAndDays (long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+//            return;
+        }
+        else{
+            long year = minutes / 525600;                                   // 525600 = minutes per year
+            long day = (minutes % 525600) / 1440;                           // 1440 = minutes per day
+            System.out.println(minutes + " min = " + year + " y and " + day + " d");
+        }
+    }
+
+    // test-3
+    public static void printEqual (int a, int b, int c) {
+
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println("Invalid Value");
+        }
+        else if ((a == b) && (b == c)) {
+            System.out.println("All numbers are equal");
+        }
+        else if ((a == b) || (a == c) || (b == c)) {
+            System.out.println("Neither all are equal or different");
+        }
+        else{
+            System.out.println("All numbers are different");
+        }
+    }
+
+    // test-4
+    // The cats spend most of the day playing. In particular, they play if the temperature is between 25 and 35 (inclusive).
+    // Unless it is summer, then the upper limit is 45 (inclusive) instead of 35.
+    public static boolean isCatPlaying (boolean summer, int temperature) {
+
+        int maxTemp = summer ? 45 : 35;
+
+        boolean res = (temperature >= 25 && temperature <= maxTemp) ? true : false;
+        return res;
+    }
 }
 
